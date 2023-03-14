99ab5b618f778c4f8dc4efdc7b5c870d59d4a31d
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2023-03-14 16:39:22 +0100

    env modifié

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  Api_URL: "http://localhost:7070/api/"
+  // Api_URL: "http://localhost:7070/api/"
+  Api_URL:'https://front-end-todo-list-pern.vercel.app:7070/api/'
+
 };
